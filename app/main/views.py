02ef210715e9c3edd7c48fd73bc92c5449863e40
Gladys Mwangi <gladysmwangi04@yahoo.com>
2022-05-15 21:54:35 +0300

    Updated the views  update post

--- app/main/views.py
+++ app/main/views.py
@@ -13,42 +13,63 @@ def index():
     name = "Time to get started "
     posts = Post.query.all()
     quote = get_random_quote()
-    # context ={
-    #     name: name
-    # }
+
     return render_template('index.html', name=name, posts = posts, quote = quote)
 
-@main.route('/signup',methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+# @main.route('/signup',methods=['POST', 'GET'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('main.index'))
     
-    form=RegistrationForm()
+#     form=RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User (username = form.username.data,
+#                     email = form.email.data,
+#                     password = hashed_password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('your account has been created successfully.')
+#         return redirect(url_for('main.login'))
+#     # else:
+#     #     flash(f'There was an error in creating the user:(err_msg)')
+#     return render_template('signUp.html', form = form)
+
+@main.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User (username = form.username.data,
-                    email = form.email.data,
-                    password = hashed_password)
+        user = User(email = form.email.data, username = form.username.data,password = hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('your account has been created successfully.')
-    return render_template('signUp.html', form = form)
+        return redirect(url_for('main.login'))
+    return render_template('signUp.html',form = form)
 
-@main.route('/signin', methods=['GET','POST'])
+# @main.route('/signin', methods=['GET','POST'])
+# def login():    
+#     form=LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user and bcrypt.check_password_hash(user.password, form.password.data):
+#             login_user(user,remember=form.remember.data)
+#             next_page = request.args.get('next')
+#             return redirect(next_page) if next_page else redirect(url_for('main.index'))
+#         else:
+#             flash('Login Unsuccessful. Please check your username and password', 'danger')
+    
+#     return render_template('signIn.html', form = form)
+
+@main.route('/login', methods = ['GET','POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    
-    form=LoginForm()
+    form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user,remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.index'))
+        user = User.query.filter_by(username = form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
         else:
-            flash('Login Unsuccessful. Please check your username and password', 'danger')
-    
+            flash('Invalid')
     return render_template('signIn.html', form = form)
 
 @main.route('/logout')
@@ -68,9 +89,21 @@ def new_post():
         return redirect(url_for('main.index'))
     
     return render_template('newPost.html', form = form)
-@main.route('/post/<int:id>/update', methods=['GET','POST'])
-def update_post(id):
+@main.route('/post/<int:post_id>/update', methods=['GET','POST'])
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
     form = UpdatePostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('main.new_post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
     
     return render_template('updatePost.html', form = form)
 
@@ -83,7 +116,7 @@ def newpitch():
 
 @main.route('/comment/<int:post_id>',methods = ['POST','GET'])
 @login_required
-def comment(pitch_id):
+def comment(post_id):
     post=Post.query.get_or_404(post_id)
     form = CommentForm()
     allComments = Comment.query.filter_by(post_id = post_id).all()
@@ -94,9 +127,9 @@ def comment(pitch_id):
         db.session.commit()
         flash('Comment added successfully')
         
-        return redirect(url_for('main.comment',pitch_id=pitch_id))
+        return redirect(url_for('main.comment',post_id=post_id))
 
-    return render_template("comment.html",pitch=pitch, title='Say something about this post', form = form,allComments=allComments)
+    return render_template("comment.html",post=post, title='Say something about this post', form = form,allComments=allComments)
 
 @main.route('/delete/comment/<comment_id>')
 @login_required
